135c4027290b2d1fb62b27abbc1f69e5bcc486ac
Srinjoy Ray <srinjoyray@yahoo.in>
2020-10-15 17:05:00 +0530

    Add solution for problem `A - Floor Number` Link: https://codeforces.com/contest/1426/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/1426/submission/95579836

--- codeforces/1426/A.cpp
+++ codeforces/1426/A.cpp
@@ -1,40 +1,21 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
-#define fo(i,n) for(i=0;i<n;i++)
-#define fo2(i,start,end) for(i=start;i<=end;i++)
-#define rfo(i,n) for(i=n-1;i>=0;i--)
-#define lli long long int
-#define deb(x) cout<<#x<<"="<<x<<endl
-#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
-#define pb push_back
-#define mp make_pair
-#define F first
-#define S second
-#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
-#define PI 3.141592653589793238
-#define mod 1e9+7
-
-void solve(){
-    int i,j;
-    int n,x,ans=1;
-    cin>>n>>x;
-    if(n<=2){
-        cout<<ans<<endl;
-        return;
-    }
-    n-=2;
-    ans+=(n+x-1)/x;
-    cout<<ans<<endl;
-
-}
-
 int main(){
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int t=1;
-    cin>>t;
-    while(t--){
-        solve();
+    int T;
+    cin>>T;
+    while(T--)
+    { int n,x,sum=0;
+      cin>>n>>x;
+      for(int i=1;;i++)
+         { if(i==1)
+             sum+=2;
+           else 
+             sum+=x;
+           if(sum>=n)
+              {cout<<i<<endl;break;}
+             
+         }
+        
     }
-    return 0;
+    
 }
-
