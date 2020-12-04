696af4942dae979a2d4e2211bdfec9db158497f4
Srinjoy Ray <srinjoyray@yahoo.in>
2020-12-04 13:49:00 +0530

    Add solution for problem `B - Queue at the School` Link: https://codeforces.com/contest/266/problem/B Tags: constructive algorithms, graph matchings, implementation, shortest paths, *800 Ref: https://codeforces.com/contest/266/submission/100332119

---
+++ codeforces/266/B.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include<iostream>
+#include<string.h>
+#include<algorithm>
+#include<math.h>
+#include<ctype.h>
+using namespace std;
+ 
+int main()
+{ int n,t;
+  cin>>n>>t;
+  string s;
+  cin>>s;
+  while(t--)
+  { for(int i=1;i<n;)
+       if(s[i]=='G' && s[i-1]=='B')
+         { s[i-1]='G';
+           s[i]='B';
+           i+=2;
+         }
+       else 
+         i++;
+  }
+  cout<<s<<endl;
+  return 0;  
+}
