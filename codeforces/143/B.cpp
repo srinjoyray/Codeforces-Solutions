6fa3e43188c47d14a325031294e8b79ad30e484c
Srinjoy Ray <srinjoyray@yahoo.in>
2020-12-18 21:02:00 +0530

    Add solution for problem `B - Help Kingdom of Far Far Away 2` Link: https://codeforces.com/contest/143/problem/B Tags: implementation, strings, *1200 Ref: https://codeforces.com/contest/143/submission/101653728

---
+++ codeforces/143/B.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<math.h>
+#include<ctype.h>
+#define lli long long int 
+#define mod 1000003
+#define pi 3.141592653589793238
+using namespace std;
+ 
+int main()
+{ string s,num,ans,dec;
+  bool neg=false;
+  int flag=0;
+  cin>>s;
+  if(s[0]=='-')
+  { neg=true;
+    s.erase(0,1);  
+  }//hello
+  for(int i=0;i<s.size();i++)
+  { if(s[i]=='.')
+      {flag=1;continue;}
+    if(flag==0)
+      num.push_back(s[i]);
+    else
+      dec.push_back(s[i]);
+    if(dec.size()==2)
+      break;
+  }
+  for(int i=num.size()-1,count=0;i>=0;i--)
+  { count++;
+    ans.push_back(num[i]);
+    if(count%3==0 && i>0)
+       ans.push_back(',');  
+  }
+  reverse(ans.begin(),ans.end());
+  if(dec.size()==0)
+    dec+="00";
+  else if(dec.size()==1)
+    dec+='0';
+
+  if(neg==true)
+    cout<<"(";
+  cout<<"$"<<ans<<"."<<dec;
+  if(neg==true)
+    cout<<")";
+  
+  return 0;  
+}
