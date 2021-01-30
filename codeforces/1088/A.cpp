27799cad461970155820009026941a2c364e7b35
Srinjoy Ray <srinjoyray@yahoo.in>
2021-01-30 20:06:00 +0530

    Add solution for problem `A - Ehab and another construction problem` Link: https://codeforces.com/contest/1088/problem/A Tags: brute force, constructive algorithms, *800 Ref: https://codeforces.com/contest/1088/submission/106029504

---
+++ codeforces/1088/A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<math.h>
+#include<ctype.h>
+#define lli long long int 
+#define mod 1000000007
+#define pi 3.141592653589793238
+using namespace std;
+
+int main()
+{   int t=1;
+    //cin>>t;
+    while(t--)
+    { int x;
+      cin>>x;
+      if(x==1)
+        cout<<"-1\n";
+      else
+        cout<<x<<" "<<x;
+    }  
+    
+    return 0;  
+}
