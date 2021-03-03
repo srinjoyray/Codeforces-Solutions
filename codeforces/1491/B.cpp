c87c7d19dec27133a43726d7162ed0ff77a6b274
Srinjoy Ray <srinjoyray@yahoo.in>
2021-03-03 19:52:00 +0530

    Add solution for problem `B - Minimal Cost` Link: https://codeforces.com/contest/1491/problem/B Tags: brute force, math, *1200 Ref: https://codeforces.com/contest/1491/submission/109014197

---
+++ codeforces/1491/B.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define lli long long int
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.141592653589793238
+#define mod 1e9+7
+void solve(){
+    int i,j;
+    int n;
+    lli u,v;
+    cin>>n>>u>>v;
+    int a[n],last;
+    bool flag=false,ok=false;
+    fo(i,n){
+        cin>>a[i];
+        if(i==0){
+            last=a[i];
+        }
+        else{
+            if(abs(a[i]-last)>=2){
+                flag=true;
+            }
+            if(abs(a[i]-last)>=1){
+                ok=true;
+            }
+            last=a[i];
+        }
+    }
+    if(flag==true){
+        cout<<"0\n";
+        return;
+    }
+    else if(ok==true){
+        cout<<min(u,v)<<nl;
+    }
+    else{
+        cout<<min(2*v,u+v)<<nl;
+    }    
+}
+
+int main(){
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
