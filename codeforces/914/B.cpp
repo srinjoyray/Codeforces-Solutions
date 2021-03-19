37ce21188ba027d9b620e87b82c8ed2ce23acb9d
Srinjoy Ray <srinjoyray@yahoo.in>
2021-03-19 20:10:00 +0530

    Add solution for problem `B - Conan and Agasa play a Card Game` Link: https://codeforces.com/contest/914/problem/B Tags: games, greedy, implementation, *1200 Ref: https://codeforces.com/contest/914/submission/110461568

---
+++ codeforces/914/B.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define lli long long int
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.141592653589793238
+#define mod 1000000007
+
+void solve(){
+    int i,j;
+    int n;
+    cin>>n;
+    int a[n],temp;
+    fo(i,n){
+        cin>>a[i];
+    }
+    sort(a,a+n,greater<int>());
+    fo(i,n){
+        j=i+1;
+        while(j<n && a[j]==a[i]){
+            j++;
+        }
+        temp=j-i;
+        i=j-1;
+        // deb(temp);
+        if(temp%2==1){
+            cout<<"Conan\n";
+            return;
+        }
+    }
+    cout<<"Agasa\n";
+
+}
+
+int main(){
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
