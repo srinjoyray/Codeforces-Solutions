f92caa0d2c43c96c95aba9e36b6f85645f37839f
Srinjoy Ray <srinjoyray@yahoo.in>
2021-03-25 20:17:00 +0530

    Add solution for problem `A - Strange Table` Link: https://codeforces.com/contest/1506/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1506/submission/110989595

---
+++ codeforces/1506/A.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define lli long long int
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.141592653589793238
+#define mod 1000000007
+
+void solve(){
+    int i,j;
+    lli n,m,x,l,r,ans;
+    cin>>n>>m>>x;
+    l=(x+n-1)/n;
+    r=x%n;
+    if(r==0){
+        r=n;
+    }
+    // deb2(l,r);
+    ans=(r-1)*m+l;
+    cout<<ans<<endl;
+
+}
+
+int main(){
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
