7541ed595b146dfd68cddba4d83a9b029f867c5c
Srinjoy Ray <srinjoyray@yahoo.in>
2021-04-13 15:19:00 +0530

    Add solution for problem `B - Fix You` Link: https://codeforces.com/contest/1391/problem/B Tags: brute force, greedy, implementation, *800 Ref: https://codeforces.com/contest/1391/submission/112911652

---
+++ codeforces/1391/B.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define lli long long int
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int>	pii;
+typedef pair<lli, lli>	pl;
+typedef vector<int>		vi;
+typedef vector<lli>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+void solve(){
+    int i,j;
+    int n,m,count=0;
+    cin>>n>>m;
+    string s[n];
+    fo(i,n){
+        cin>>s[i];
+    }
+
+    for(j=0;j<m;j++){
+        if(s[n-1][j]=='D'){
+            count++;
+        }
+    }
+    for(i=0;i<n;i++){
+        if(s[i][m-1]=='R'){
+            count++;
+        }
+    }
+    cout<<count<<nl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
