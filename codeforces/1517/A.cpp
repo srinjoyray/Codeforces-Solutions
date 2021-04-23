1da9b8d14bc9719ca89ddd91bf0df3cd0e516f21
Srinjoy Ray <srinjoyray@yahoo.in>
2021-04-23 20:09:00 +0530

    Add solution for problem `A - Sum of 2050` Link: https://codeforces.com/contest/1517/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1517/submission/113987287

---
+++ codeforces/1517/A.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define lli long long int
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int>	pii;
+typedef pair<lli, lli>	pl;
+typedef vector<int>		vi;
+typedef vector<lli>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+void solve(){
+    lli i,j;
+    lli n,start,count=0;
+    cin>>n;
+    start=2050;
+    while(n>=2050){
+        j=start;
+        while(j<=n){
+           j*=10;
+        }
+        j/=10;
+       
+        count+=n/j;
+        n=n%j;
+    }
+    if(n==0){
+        cout<<count<<nl;
+    }
+    else{
+        cout<<"-1\n";
+    }
+    
+
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
