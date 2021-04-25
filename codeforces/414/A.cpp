74cabe0a9ace613260e0053149dee0dff7a9822a
Srinjoy Ray <srinjoyray@yahoo.in>
2021-04-25 22:22:00 +0530

    Add solution for problem `A - Mashmokh and Numbers` Link: https://codeforces.com/contest/414/problem/A Tags: constructive algorithms, number theory, *1500 Ref: https://codeforces.com/contest/414/submission/114221629

---
+++ codeforces/414/A.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define lli long long int
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int>	pii;
+typedef pair<lli, lli>	pl;
+typedef vector<int>		vi;
+typedef vector<lli>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+void solve(){
+    lli i,j;
+    lli n,k,sum,rem,a,b;
+    cin>>n>>k;
+    sum=n/2;
+    if(n==1 && k==0){
+        cout<<"1\n";
+        return;
+    }
+    if(k<sum || n==1){
+        cout<<"-1\n";
+        return;
+    }
+    rem=k-sum+1;
+    a=rem; b=2*rem;
+    cout<<a<<" "<<b<<" ";
+    j=1;
+    fo(i,n/2-1){
+        if(j==a || j+1==a){
+            j+=2;
+        }
+        if(j==b || j+1==b){
+            j+=2;
+        }
+        cout<<j<<" "<<j+1<<" ";
+        j+=2;
+    }
+    if(n%2==1){
+        cout<<"1000000000";
+    }
+    cout<<nl;
+
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
