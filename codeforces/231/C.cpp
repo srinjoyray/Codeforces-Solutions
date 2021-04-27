635ab23e4a34e11de545662576fed1124e49ed41
Srinjoy Ray <srinjoyray@yahoo.in>
2021-04-27 12:34:00 +0530

    Add solution for problem `C - To Add or Not to Add` Link: https://codeforces.com/contest/231/problem/C Tags: binary search, sortings, two pointers, *1600 Ref: https://codeforces.com/contest/231/submission/114336817

---
+++ codeforces/231/C.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define lli long long int
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int>	pii;
+typedef pair<lli, lli>	pl;
+typedef vector<int>		vi;
+typedef vector<lli>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+void solve(){
+    int i,j;
+    lli n,k,count=0,temp,mx=0,val;
+    cin>>n>>k;
+    lli a[n];
+    fo(i,n){
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    /*fo(i,n){
+        cout<<a[i]<<" ";
+    }cout<<nl;*/
+    i=0;
+    j=0;
+    mx=1;
+    val=a[0];
+    temp=1;
+    count=0;
+    while(j<n){
+        // deb2(i,j);
+        // deb2(count,temp);
+        if(count<=k){
+            if(temp>mx){
+                mx=temp;
+                val=a[j];
+            }
+            j++;
+            temp++;
+            count+=(a[j]-a[j-1])*(temp-1);
+        }
+        else if(count>k){
+            count-=a[j]-a[i];
+            i++;
+            temp--;
+        }
+    }
+    cout<<mx<<" "<<val<<nl;
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
