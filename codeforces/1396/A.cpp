57ad67daca641ed9f0c4c36db281d2cc37c4ac84
Srinjoy Ray <srinjoyray@yahoo.in>
2021-05-03 18:10:00 +0530

    Add solution for problem `A - Multiples of Length` Link: https://codeforces.com/contest/1396/problem/A Tags: constructive algorithms, greedy, number theory, *1600 Ref: https://codeforces.com/contest/1396/submission/115033502

---
+++ codeforces/1396/A.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define lli long long int
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int>	pii;
+typedef pair<lli, lli>	pl;
+typedef vector<int>		vi;
+typedef vector<lli>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+void solve(){
+    int i,j;
+    int n;
+    cin>>n;
+    lli a[n],rem;
+    fo(i,n){
+        cin>>a[i];
+    }
+    if(n==1){
+        cout<<"1 1\n";
+        cout<<-a[0]<<nl;
+        cout<<"1 1\n";
+        cout<<0<<nl;
+        cout<<"1 1\n";
+        cout<<0<<nl;
+        return;
+    }
+    cout<<n<<" "<<n<<nl;
+    cout<<n-a[n-1]%n<<nl;
+    a[n-1]+=n-a[n-1]%n;
+    cout<<"1 "<<n-1<<nl;
+    fo(i,n-1){
+        rem=a[i]%(n);
+        cout<<rem*(n-1)<<" ";
+        a[i]+=rem*(n-1);
+    }
+    cout<<nl;
+    cout<<"1 "<<n<<nl;
+    fo(i,n){
+        cout<<-a[i]<<" ";
+    }cout<<nl;
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
