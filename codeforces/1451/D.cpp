72a16706fe5a87094101928139daf7608fcb6148
Srinjoy Ray <srinjoyray@yahoo.in>
2021-05-05 01:35:00 +0530

    Add solution for problem `D - Circle Game` Link: https://codeforces.com/contest/1451/problem/D Tags: games, geometry, math, *1700 Ref: https://codeforces.com/contest/1451/submission/115148329

---
+++ codeforces/1451/D.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define lli long long int
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int>	pii;
+typedef pair<lli, lli>	pl;
+typedef vector<int>		vi;
+typedef vector<lli>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+void solve(){
+    int i,j;
+    lli d,k;
+    double p,x,y,dist;
+    lli pt;
+    cin>>d>>k;
+    p=d/(sqrt(2));
+    pt=floor(p);
+    // deb2(pt,p);
+    pt-=pt%k;
+    x=pt+k; y=pt;
+    // deb2(x,y);
+    dist=sqrt(x*x +y*y);
+    // deb(dist);
+    if(dist<=d){
+        cout<<"Ashish\n";
+    }
+    else{
+        cout<<"Utkarsh\n";
+    }
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
