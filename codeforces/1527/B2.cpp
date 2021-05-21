ab38478799bae8af78104f0dadefff41151291f2
Srinjoy Ray <srinjoyray@yahoo.in>
2021-05-21 12:20:00 +0530

    Add solution for problem `B2 - Palindrome Game (hard version)` Link: https://codeforces.com/contest/1527/problem/B2 Tags: constructive algorithms, dp, games, *1900 Ref: https://codeforces.com/contest/1527/submission/116887645

---
+++ codeforces/1527/B2.cpp
@@ -0,0 +1,92 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define lli long long int
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int>	pii;
+typedef pair<lli, lli>	pl;
+typedef vector<int>		vi;
+typedef vector<lli>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+void solve(){
+    int i,j;
+    int n,count=0,diff=0,same=0;
+    cin>>n;
+    string s;
+    cin>>s;
+    bool flag=false;
+    fo(i,n){
+        if(s[i]=='0'){
+            count++;
+        }
+    }
+
+    if(n%2==1 && s[n/2]=='0'){
+        flag=true;
+    }
+
+    i=0; j=n-1;
+    while(i<j){
+        if(s[i]!=s[j]){
+            diff++;
+        }
+        else{
+            same++;
+        }
+        i++;
+        j--;
+    }
+    // deb2(diff,count);
+    if(diff>0){
+        if(flag && count==2){
+            cout<<"DRAW\n";
+        }
+        else{
+            cout<<"ALICE\n";
+        }
+        return;
+    }
+
+    
+    
+    if(flag){
+        if(count==1){
+            cout<<"BOB\n";
+        }
+        else{
+            cout<<"ALICE\n";
+        }
+       
+        return;
+    }
+
+    cout<<"BOB\n";
+    
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
