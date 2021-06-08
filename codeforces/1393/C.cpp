f73c43c25a17e7a40c90acd6617fa75f758d565d
Srinjoy Ray <srinjoyray@yahoo.in>
2021-06-08 22:48:00 +0530

    Add solution for problem `C - Pinkie Pie Eats Patty-cakes` Link: https://codeforces.com/contest/1393/problem/C Tags: constructive algorithms, greedy, math, sortings, *1700 Ref: https://codeforces.com/contest/1393/submission/118828212

---
+++ codeforces/1393/C.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define ll long long int
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+void solve(){
+    int i,j;
+    int n;
+    cin>>n;
+    int a[n],freq[n+1]={0};
+    fo(i,n){
+        cin>>a[i];
+        freq[a[i]]++;
+    }
+    int mx=0,count=0,rem,gap;
+    mx=*max_element(freq,freq+1+n);
+    fo2(i,1,n){
+        if(freq[i]==mx){
+            count++;
+        }
+    }
+    // deb2(mx,count);
+    rem=n-(count*mx);
+    gap=rem/(mx-1)+count-1;
+    cout<<gap<<nl;
+
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
+
