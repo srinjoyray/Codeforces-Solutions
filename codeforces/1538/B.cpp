e4cda5e64c7ff2416d680aeb573d631f0eae0275
Srinjoy Ray <srinjoyray@yahoo.in>
2021-06-11 02:12:00 +0530

    Add solution for problem `B - Friends and Candies` Link: https://codeforces.com/contest/1538/problem/B Tags: greedy, math Ref: https://codeforces.com/contest/1538/submission/119098015

---
+++ codeforces/1538/B.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define ll long long int
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+void solve(){
+    int i,j;
+    int n;
+    cin>>n;
+    int a[n],sum=0;
+    fo(i,n){
+        cin>>a[i];
+        sum+=a[i];
+    }
+    sort(a,a+n,greater<int>());
+    if(sum%n != 0){
+        cout<<"-1\n";
+        return;
+    }
+    sum/=n;
+    fo(i,n){
+        if(a[i]<=sum){
+            cout<<i<<nl;
+            return;
+        }
+    }
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
+
