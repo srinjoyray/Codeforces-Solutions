63bc44891aceecace33beb0dab384a35aca8a582
Srinjoy Ray <srinjoyray@yahoo.in>
2021-06-14 14:44:00 +0530

    Add solution for problem `D - TediousLee` Link: https://codeforces.com/contest/1369/problem/D Tags: dp, graphs, greedy, math, trees, *1900 Ref: https://codeforces.com/contest/1369/submission/119437159

---
+++ codeforces/1369/D.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define ll long long int
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+const int N=2e6+10;
+ll ans[N];
+void solve(){
+    int i,j;
+    int n;
+    cin>>n;
+    // deb(n);
+    cout<<(ans[n]*4)%mod<<nl;
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+   
+    ll zero=1,single=0,temp,i;
+    ans[1]=0;
+    fo2(i,2,N-1){
+        ans[i]=single;
+        if(i>3){
+            ans[i]+=ans[i-3];
+        }
+        ans[i]%=mod;
+        // deb2(i,ans[i]);
+        temp=zero*1 + single*2;
+        single=zero;
+        zero=temp;
+        zero%=mod;
+        single%=mod;
+    }
+
+    while(t--){
+        solve();
+    }
+}
+
