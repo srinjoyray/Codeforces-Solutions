94ed32046635cc88d16b6e930049045e3dfe01cb
Srinjoy Ray <srinjoyray@yahoo.in>
2021-06-25 23:55:00 +0530

    Add solution for problem `B - Pleasant Pairs` Link: https://codeforces.com/contest/1541/problem/B Tags: brute force, implementation, math, number theory, *1200 Ref: https://codeforces.com/contest/1541/submission/120611962

---
+++ codeforces/1541/B.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define ll long long int
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+void solve(){
+    int i,j;
+    int n;
+    cin>>n;
+    ll a[n+1],start,rem,ans=0;
+    fo2(i,1,n){
+        cin>>a[i];
+    }
+    fo2(i,1,n){
+        start=a[i]-i;
+        // deb(start);
+        while(start<=i){
+            start+=a[i];
+        }
+        for(j=start;j<=n;j+=a[i]){
+            if(a[i]*a[j]==i+j){
+                ans++;
+            }
+        }
+    }
+    cout<<ans<<nl;
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
+
