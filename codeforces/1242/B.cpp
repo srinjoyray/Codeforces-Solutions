b800171d1e55e6276deac8b8e7e4c3a2cfe9001a
Srinjoy Ray <srinjoyray@yahoo.in>
2021-06-25 12:17:00 +0530

    Add solution for problem `B - 0-1 MST` Link: https://codeforces.com/contest/1242/problem/B Tags: dfs and similar, dsu, graphs, sortings, *1900 Ref: https://codeforces.com/contest/1242/submission/120497201

---
+++ codeforces/1242/B.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define ll long long int
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+const int N=1e6;
+
+void solve(){
+    int i,j;
+    int n,m,x,y,ans=0;
+    cin>>n>>m;
+    set<pii> edges;
+    fo(i,m){
+        cin>>x>>y;
+        edges.insert({x,y});
+        edges.insert({y,x});
+    }
+    set<int> unvisited;
+    fo2(i,1,n){
+        unvisited.insert(i);
+    }
+
+    fo2(i,1,n){
+        if(unvisited.count(i)==0){
+            continue;
+        }
+        ans++;
+        vector<int> curr;
+        curr.pb(i);
+        unvisited.erase(i);
+        fo(j,curr.size()){
+            int node=curr[j];
+            vi vis;
+            for(auto child : unvisited){
+                if(edges.count({node,child})==0){
+                    curr.pb(child);
+                    vis.pb(child);
+                }
+            }
+            for(auto u : vis){
+                unvisited.erase(u);
+            }
+        }
+    }
+    cout<<ans-1<<nl;
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+}
+
