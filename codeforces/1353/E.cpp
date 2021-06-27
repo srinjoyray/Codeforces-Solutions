bd98c7f1141d8877d08bd721bf1f03ee6f7649c7
Srinjoy Ray <srinjoyray@yahoo.in>
2021-06-27 18:21:00 +0530

    Add solution for problem `E - K-periodic Garland` Link: https://codeforces.com/contest/1353/problem/E Tags: brute force, dp, greedy, *1900 Ref: https://codeforces.com/contest/1353/submission/120735529

---
+++ codeforces/1353/E.cpp
@@ -0,0 +1,146 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define ll long long int
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+const int N=1e6;
+
+/*bool prime[N + 1];
+void sieveOfEratosthenes()
+{
+    memset(prime, true, sizeof(prime));
+    for (int p = 2; p * p <= N; p++)
+    {
+        if (prime[p] == true){
+            for (int i = p * p; i <= N; i += p)
+                prime[i] = false;
+        }
+    }
+    prime[0]=false; prime[1]=false;
+}
+
+int spf[N];
+void sieve()
+{
+    spf[1] = 1;
+    for (int i=2; i<N; i++)
+        spf[i] = i;
+
+    for (int i=4; i<N; i+=2)
+        spf[i] = 2;
+ 
+    for (int i=3; i*i<N; i++){
+        if (spf[i] == i){
+            for (int j=i*i; j<N; j+=i)
+                if (spf[j]==j)
+                    spf[j] = i;
+        }
+    }
+}
+
+string to_binary(int n){
+    int binaryNum[32]={0};
+    string s;
+    int i=0,j;
+    while(n>0){
+        binaryNum[i] = n % 2;
+        n=n/2;
+        i++;
+    }
+    fo(j,32){
+        s.pb(binaryNum[j]+'0');
+    }
+    return s;
+}
+
+ll power(ll a,ll n){
+    ll res=1;
+    while(n){
+        if(n%2){
+            res=(res*a)%mod;
+        }
+        a=(a*a)%mod;
+        n/=2;
+    }
+    return res;
+}*/
+
+int check(vi v){
+    int i,curr,temp,ans,n;
+    curr=0;
+    n=v.size();
+    if(n<=1){
+        return 0;
+    }
+    ans=n-1;
+    // fo(i,v.size()){
+    //     cout<<v[i];
+    // }
+    fo2(i,1,v.size()-1){
+        temp=v[i]-v[i-1]-1;
+        curr=min(curr+temp,i);
+        ans=min(ans,curr+n-1-i);
+    }
+    
+    return ans;
+}
+
+void solve(){
+    int i,j;
+    int n,k,start,count=0,ans=INT_MAX,temp;
+    cin>>n>>k;
+    string s;
+    cin>>s;
+    fo(i,n){
+        if(s[i]=='1'){
+            count++;
+        }
+    }
+
+    for(start=0;start<k;start++){
+        vi v;
+        for(i=start;i<n;i+=k){
+            // deb(i);
+            if(s[i]=='1'){
+                v.pb(i/k);
+            }
+        }
+        temp=count-v.size();
+        // deb(temp);
+        temp+=check(v);
+        // deb(temp);
+        ans=min(ans,temp);
+    }
+    cout<<ans<<nl;
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
+
