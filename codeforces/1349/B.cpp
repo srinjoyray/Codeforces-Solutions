741198eb6bbc22c88fa13035e003eff107953312
Srinjoy Ray <srinjoyray@yahoo.in>
2021-07-13 17:12:00 +0530

    Add solution for problem `B - Orac and Medians` Link: https://codeforces.com/contest/1349/problem/B Tags: constructive algorithms, greedy, math, *2000 Ref: https://codeforces.com/contest/1349/submission/122319948

---
+++ codeforces/1349/B.cpp
@@ -0,0 +1,174 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define ll long long int
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+const int N=1e6+10;
+
+/*bool prime[N + 1];
+void sieveOfEratosthenes()
+{
+    memset(prime, true, sizeof(prime));
+    for (int p = 2; p * p <= N; p++)
+    {
+        if (prime[p] == true){
+            for (int i = p * p; i <= N; i += p)
+                prime[i] = false;
+        }
+    }
+    prime[0]=false; prime[1]=false;
+}*/
+
+/*int spf[N];
+void sieve()
+{
+    spf[1] = 1;
+    for (int i=2; i<N; i++)
+        spf[i] = i;
+
+    for (int i=4; i<N; i+=2)
+        spf[i] = 2;
+ 
+    for (int i=3; i*i<N; i++){
+        if (spf[i] == i){
+            for (int j=i*i; j<N; j+=i)
+                if (spf[j]==j)
+                    spf[j] = i;
+        }
+    }
+}*/
+
+/*string to_binary(int n){
+    int binaryNum[32]={0};
+    string s;
+    int i=0,j;
+    while(n>0){
+        binaryNum[i] = n % 2;
+        n=n/2;
+        i++;
+    }
+    fo(j,32){
+        s.pb(binaryNum[j]+'0');
+    }
+    reverse(all(s));
+    return s;
+}*/
+
+/*ll power(ll a,ll n){
+    ll res=1;
+    while(n){
+        if(n%2){
+            res=(res*a)%mod;
+        }
+        a=(a*a)%mod;
+        n/=2;
+    }
+    return res;
+}*/
+bool check(vl v,ll k){
+    int i,j,n,start;
+    n=v.size();
+    fo(i,n){
+        if(v[i]==k){
+            start=i;
+            break;
+        }
+    }
+    int good=1,bad=0;
+    for(i=start+1;i<n;i++){
+        if(v[i]>=k){
+            good++;
+        }
+        else{
+            bad++;
+        }
+        
+        if(good>bad && good+bad>1){
+            return true;
+        }
+
+        if(v[i]==k){
+            good=1;
+            bad=0;
+        }
+    }
+    return false;
+}
+
+void solve(){
+    int i,j;
+    int n;
+    ll k;
+    cin>>n>>k;
+    vl v(n);
+    bool flag=true;
+    fo(i,n){
+        cin>>v[i];
+        if(v[i]==k){
+            flag=false;
+        }
+    }
+    if(flag){
+        cout<<"no\n";
+        return;
+    }
+    if(n==1){
+        cout<<"yes\n";
+        return;
+    }
+    // if(check(v,k)){
+    //     cout<<"yes\n";
+    //     return;
+    // }
+    // reverse(all(v));
+    // if(check(v,k)){
+    //     cout<<"yes\n";
+    //     return;
+    // }
+
+    fo(i,n-1){
+        if(v[i]>=k && v[i+1]>=k){
+            cout<<"yes\n";
+            return;
+        }
+    }
+    fo(i,n-2){
+        if(v[i]>=k && v[i+2]>=k){
+            cout<<"yes\n";
+            return;
+        }
+    }
+    cout<<"no\n";
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    
+}
+
