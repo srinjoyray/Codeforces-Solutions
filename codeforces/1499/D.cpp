038938fbe12acf45deb3b3edad61c99e137ae430
Srinjoy Ray <srinjoyray@yahoo.in>
2021-07-22 12:39:00 +0530

    Add solution for problem `D - The Number of Pairs` Link: https://codeforces.com/contest/1499/problem/D Tags: dp, math, number theory, *2100 Ref: https://codeforces.com/contest/1499/submission/123250418

---
+++ codeforces/1499/D.cpp
@@ -0,0 +1,145 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define ll long long int
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+const int N=3e7+10;
+
+/*bool prime[N + 1];
+void sieveOfEratosthenes()
+{
+    memset(prime, true, sizeof(prime));
+    for (int p = 2; p * p <= N; p++)
+    {
+        if (prime[p] == true){
+            for (int i = p * p; i <= N; i += p)
+                prime[i] = false;
+        }
+    }
+    prime[0]=false; prime[1]=false;
+}*/
+
+
+/*string to_binary(int n){
+    int binaryNum[32]={0};
+    string s;
+    int i=0,j;
+    while(n>0){
+        binaryNum[i] = n % 2;
+        n=n/2;
+        i++;
+    }
+    fo(j,32){
+        s.pb(binaryNum[j]+'0');
+    }
+    reverse(all(s));
+    return s;
+}*/
+
+ll power(ll a,ll n){
+    ll res=1;
+    while(n){
+        if(n%2){
+            res=(res*a)%mod;
+        }
+        a=(a*a)%mod;
+        n/=2;
+    }
+    return res;
+}
+
+int spf[N];
+void sieve()
+{
+    spf[1] = 1;
+    for (int i=2; i<N; i++)
+        spf[i] = i;
+
+    for (int i=4; i<N; i+=2)
+        spf[i] = 2;
+ 
+    for (int i=3; i*i<N; i++){
+        if (spf[i] == i){
+            for (int j=i*i; j<N; j+=i)
+                if (spf[j]==j)
+                    spf[j] = i;
+        }
+    }
+}
+ll find(ll val){
+    ll temp,count=0;
+    while(spf[val]!=val){
+        count++;
+        temp=spf[val];
+        while(val%temp==0){
+            val/=temp;
+        }
+    }
+    if(val>1){
+        count++;
+    }
+    return count;
+}
+void solve(){
+    int i,j;
+    ll c,d,x;
+    cin>>c>>d>>x;
+    // deb2(c,d);
+    vi fact;
+    for(i=1;i*i<=x;i++){
+        if(x%i==0){
+            fact.pb(i);
+            if(i*i!=x){
+                fact.pb(x/i);
+            }
+            
+        }
+    }
+    ll val,ans=0,temp;
+    fo(i,fact.size()){
+        val=x/fact[i];
+        // deb(val);
+        val+=d;
+        if(val%c==0){
+            val/=c;
+            // deb(val);
+            temp=find(val);
+            // deb(temp);
+            ans+=power(2,temp);
+        }
+    }
+    cout<<ans<<nl;
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+    sieve();
+    while(t--){
+        solve();
+    }
+    
+}
+
