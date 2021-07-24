168c30953fde82a9999c2577f1e4a44acf5531f3
Srinjoy Ray <srinjoyray@yahoo.in>
2021-07-24 09:28:00 +0530

    Add solution for problem `D1 - Domino (easy version)` Link: https://codeforces.com/contest/1551/problem/D1 Tags: constructive algorithms, math, *1700 Ref: https://codeforces.com/contest/1551/submission/123566739

---
+++ codeforces/1551/D1.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define ll long long int
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+const int N=1e6+10;
+
+void solve(){
+    int i,j;
+    int n,m,k,val,rem,hor,vert;
+    cin>>n>>m>>k;
+    hor=k;
+    vert=(m*n)/2-k;
+    if(n%2==1){
+        hor-=m/2;
+    }
+    if(m%2==1){
+        vert-=n/2;
+    }
+    if(hor>=0 && vert>=0 && hor%2==0 && vert%2==0){
+        cout<<"YES\n";
+    }
+    else{
+        cout<<"NO\n";
+    }
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    
+}
+
