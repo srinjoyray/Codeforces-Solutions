c29c090735d0fa25e647cbf8b2ee6e7c11edd470
Srinjoy Ray <srinjoyray@yahoo.in>
2021-08-01 15:00:00 +0530

    Add solution for problem `E - Boring Segments` Link: https://codeforces.com/contest/1555/problem/E Tags: data structures, sortings, trees, two pointers, *2100 Ref: https://codeforces.com/contest/1555/submission/124492816

--- codeforces/1555/E.cpp
+++ codeforces/1555/E.cpp
@@ -1,31 +1,13 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
 using namespace std;
-#define fo(i,n) for(i=0;i<n;i++)
-#define fo2(i,start,end) for(i=start;i<=end;i++)
-#define rfo(i,n) for(i=n-1;i>=0;i--)
-#define ll long long int
-#define deb(x) cout<<#x<<"="<<x<<endl
-#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
-#define pb push_back
-#define mp make_pair
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-#define sortall(x) sort(all(x))
-#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
-#define nl '\n'
-#define PI 3.1415926535897932384626
-#define mod 1000000007
-typedef pair<int, int>	pii;
-typedef pair<ll, ll>	pl;
-typedef vector<int>		vi;
-typedef vector<ll>		vl;
-typedef vector<pii>		vpii;
-typedef vector<pl>		vpl;
+
 const int INF = 1e9;
- 
+
 vector<int> t, ps;
- 
+
 void push(int v){
 	if (v * 2 + 1 < int(ps.size())){
 		ps[v * 2] += ps[v];
@@ -49,34 +31,33 @@ void upd(int v, int l, int r, int L, int R, int val){
 	upd(v * 2 + 1, m, r, max(m, L), R, val);
 	t[v] = min(t[v * 2], t[v * 2 + 1]);
 }
+
 int get(){
 	return t[1] + ps[1];
 }
+
 struct seg{
 	int l, r, w;
 };
-void solve(){
-    int i,j;
-    int n,m;
-    cin>>n>>m;
-    vector<seg> a(n);
-    fo(i,n){
-        cin>>a[i].l>>a[i].r>>a[i].w;
-        a[i].l--;
-        a[i].r--;
-    }
-    m--;
-    sort(a.begin(), a.end(), [](const seg &a, const seg &b){
+
+int main() {
+	int n, m;
+	scanf("%d%d", &n, &m);
+	vector<seg> a(n);
+	forn(i, n){
+		scanf("%d%d%d", &a[i].l, &a[i].r, &a[i].w);
+		--a[i].l, --a[i].r;
+	}
+	--m;
+	sort(a.begin(), a.end(), [](const seg &a, const seg &b){
 		return a.w < b.w;
 	});
-    
-    t.resize(4 * m);
+	t.resize(4 * m);
 	ps.resize(4 * m);
-
-    int ans = INF;
-    j=0;
-    fo(i,n){
-        while (j < n && get() == 0){
+	int ans = INF;
+	int j = 0;
+	forn(i, n){
+		while (j < n && get() == 0){
 			upd(1, 0, m, a[j].l, a[j].r, 1);
 			++j;
 		}
@@ -85,19 +66,7 @@ void solve(){
 		}
 		ans = min(ans, a[j - 1].w - a[i].w);
 		upd(1, 0, m, a[i].l, a[i].r, -1);
-    }
-    cout<<ans<<nl;
+	}
+	printf("%d\n", ans);
+	return 0;
 }
-
-int main(){
-    // freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int t=1;
-    // cin>>t;
-    while(t--){
-        solve();
-    }
-    
-}
-
