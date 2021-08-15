a5ec20ab05cabdd8857bd242a43a073ed60f861f
Srinjoy Ray <srinjoyray@yahoo.in>
2021-08-15 20:07:00 +0530

    Add solution for problem `A - Mocha and Math` Link: https://codeforces.com/contest/1559/problem/A Tags: bitmasks, constructive algorithms, math, *900 Ref: https://codeforces.com/contest/1559/submission/125935085

---
+++ codeforces/1559/A.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define ll long long int
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+const int N=1e6+10;
+
+void solve(){
+    int i,j;
+    int n;
+    cin>>n;
+    int a[n];
+    fo(i,n){
+        cin>>a[i];
+    }
+    int ans=a[0];
+    fo(i,n){
+        ans=ans&a[i];
+    }
+    cout<<ans<<nl;
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    
+}
+
