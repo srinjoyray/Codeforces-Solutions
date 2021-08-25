79d40a4708916614f8200856c139a1f93ff39faf
Srinjoy Ray <srinjoyray@yahoo.in>
2021-08-25 15:39:00 +0530

    Add solution for problem `D1 - Up the Strip (simplified version)` Link: https://codeforces.com/contest/1561/problem/D1 Tags: brute force, data structures, dp, math, number theory, *1700 Ref: https://codeforces.com/contest/1561/submission/126958392

---
+++ codeforces/1561/D1.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define ll long long int
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+const int N=1e6+10;
+
+void solve(){
+    int i,j;
+    ll n,m;
+    cin>>n>>m;
+    ll dp[n+1]={0},temp,count,ans=0,root,val,start,sum=1,rem,end;
+    dp[1]=1;
+    fo2(i,2,n){
+        dp[i]=sum;
+        root=sqrt(i);
+        for(j=2;j<=root;j++){
+            val=i/j;
+            dp[i]=(dp[i]+dp[val])%m;
+        }
+        // deb(i);
+        rem=i;
+        // deb(dp[i]);
+        for(j=1;rem>root;j++){
+            if(j*j==i){
+                continue;
+            }
+            // deb(j);
+            end=(i/(j+1));
+            count=rem-end;
+            rem=end;
+            // deb2(rem,count);
+            dp[i]=(dp[i]+(dp[j]*count)%m)%m;
+        }
+        // deb(dp[i]);
+        sum+=dp[i];
+        sum%m;
+    }
+    
+    cout<<dp[n]<<nl;
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    
+}
+
