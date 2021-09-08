5f84225693ecac5f1d77598b67e2f391b916a35c
Srinjoy Ray <srinjoyray@yahoo.in>
2021-09-08 19:01:00 +0530

    Add solution for problem `C - Ayoub's function` Link: https://codeforces.com/contest/1301/problem/C Tags: binary search, combinatorics, greedy, math, strings, *1700 Ref: https://codeforces.com/contest/1301/submission/128207848

---
+++ codeforces/1301/C.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define ll long long int
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+const int N=1e6+10;
+
+void solve(){
+    int i,j;
+    ll n,m,ans,zero=0,group,less,more,extra,temp;
+    cin>>n>>m;
+    group=m+1;
+    zero=n-m;
+    // deb2(group,zero);
+    less=zero/group;
+    more=less+1;
+    extra=(zero%group);
+    // deb2(extra,more);
+    temp=extra*((more*(more+1))/2) + (group-extra)*((less*(less+1))/2);
+    // deb(temp);
+    ans=(n*(n+1)/2) - temp;
+
+    cout<<ans<<nl;
+
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    
+}
+
