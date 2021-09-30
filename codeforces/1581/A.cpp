10c5cc342e7d7fd4c6a70bde2f4c3739a22c0d04
Srinjoy Ray <srinjoyray@yahoo.in>
2021-09-30 15:49:00 +0530

    Add solution for problem `A - CQXYM Count Permutations` Link: https://codeforces.com/contest/1581/problem/A Tags: combinatorics, math, number theory, *800 Ref: https://codeforces.com/contest/1581/submission/130338840

---
+++ codeforces/1581/A.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define ll long long int
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+const int N=1e6+10;
+ll fact[N];
+ll power(ll a,ll n){
+    ll res=1;
+    while(n){
+        if(n%2){
+            res=(res*a)%mod;
+        }
+        a=(a*a)%mod;
+        n/=2;
+    }
+    return res;
+}
+void solve(){
+    int i,j;
+    int n;
+    cin>>n;
+    ll ans,denom;
+    ans=fact[2*n];
+    // deb(ans);
+    denom=power(2,mod-2);
+    ans=(ans*denom)%mod;
+    cout<<ans<<nl;
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+    fact[0]=1;
+    for(int i=1;i<N;i++){
+        fact[i]=(fact[i-1]*i)%mod;
+    }
+    while(t--){
+        solve();
+    }
+    
+}
+
