fe88bf658f6e4ca85f0eb6974006c583a1ee8ba1
Srinjoy Ray <srinjoyray@yahoo.in>
2021-10-02 23:57:00 +0530

    Add solution for problem `E2 - Array Optimization by Deque` Link: https://codeforces.com/contest/1579/problem/E2 Tags: data structures, greedy, *1700 Ref: https://codeforces.com/contest/1579/submission/130591596

---
+++ codeforces/1579/E2.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef tree<pair<ll, ll>, null_type, less<pair<ll, ll>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+const int N=1e6+10;
+
+void solve(){
+    int i,j;
+    pbds A;
+    int n;
+    cin>>n;
+    ll a[n],idx1,idx2,more,same,less,ans=0;
+    fo(i,n){
+        cin>>a[i];
+    }
+    fo(i,n){
+        A.insert({a[i],i});
+        idx1=A.order_of_key({a[i]-1,n+1});
+        idx2=A.order_of_key({a[i],n+1});
+        same=idx2-idx1;
+        less=idx1;
+        // deb2(idx1,idx2);
+        more=(i+1)-same-less;
+        // deb2(more,less);
+        ans+=min(less,more);
+    }
+    cout<<ans<<nl;
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    
+}
+
