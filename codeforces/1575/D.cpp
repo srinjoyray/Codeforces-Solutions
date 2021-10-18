175ba9626808a3b80d6680fbd533a9921f7b7473
Srinjoy Ray <srinjoyray@yahoo.in>
2021-10-18 20:16:00 +0530

    Add solution for problem `D - Divisible by Twenty-Five` Link: https://codeforces.com/contest/1575/problem/D Tags: brute force, dfs and similar, dp, *1800 Ref: https://codeforces.com/contest/1575/submission/132359939

---
+++ codeforces/1575/D.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+const int N=1e6+10;
+bool comp(string s,string temp){
+    int i,n,last=-1;
+    n=s.size();
+    fo(i,n){
+        if(s[i]=='_'){
+            continue;
+        }
+        else if(s[i]=='X'){
+            if(last==-1){
+                last=temp[i]-'0';
+            }
+            else if(last!=temp[i]-'0'){
+                return false;
+            }
+        }
+        else if(s[i]!=temp[i]){
+            return false;
+        }
+    }
+    
+    return true;
+}
+void solve(){
+    int i,j;
+    string s,temp;
+    cin>>s;
+    int n,start,end,ans=0; 
+    n=s.size();
+    start=pow(10,n-1);
+    end=pow(10,n)-1;
+    if(start==1){
+        start=0;
+    }
+    while(start%25!=0){
+        start++;
+    }
+    // deb2(start,end);
+    while(start<=end){
+        temp=to_string(start);
+        if(comp(s,temp)){
+            // deb(start);
+            ans++;
+        }
+        start+=25;
+    }
+    cout<<ans<<nl;
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    
+}
+
