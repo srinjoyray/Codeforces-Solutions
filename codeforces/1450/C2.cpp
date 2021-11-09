98a8a0ba86fc37e52ba87131a0fbddb12ba2ffbb
Srinjoy Ray <srinjoyray@yahoo.in>
2021-11-09 20:51:00 +0530

    Add solution for problem `C2 - Errich-Tac-Toe (Hard Version)` Link: https://codeforces.com/contest/1450/problem/C2 Tags: constructive algorithms, math, *2300 Ref: https://codeforces.com/contest/1450/submission/134773088

---
+++ codeforces/1450/C2.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+const int N=1e6+10;
+
+void solve(){
+    int i,j;
+    int n;
+    cin>>n;
+    string s[n];
+    int freq1[3]={0},freq2[3]={0},k=0;
+    fo(i,n){
+        cin>>s[i];
+    }
+    fo(i,n){
+        fo(j,n){
+            if(s[i][j]=='X'){
+                k++;
+                freq1[(i+j)%3]++;
+            }
+            if(s[i][j]=='O'){
+                k++;
+                freq2[(i+j)%3]++;
+            }
+        }
+    }
+    pii p;
+    if(freq1[0]+freq2[1]<=k/3){
+        p={0,1};
+    }
+    else if(freq1[1]+freq2[0]<=k/3){
+        p={1,0};
+    }
+    else if(freq1[0]+freq2[2]<=k/3){
+        p={0,2};
+    }
+    else if(freq1[2]+freq2[0]<=k/3){
+        p={2,0};
+    }
+    else if(freq1[1]+freq2[2]<=k/3){
+        p={1,2};
+    }
+    else if(freq1[2]+freq2[1]<=k/3){
+        p={2,1};
+    }
+
+    fo(i,n){
+        fo(j,n){
+            if(s[i][j]=='X' && (i+j)%3 == p.F){
+                s[i][j]='O';
+            }
+            if(s[i][j]=='O' && (i+j)%3 == p.S){
+                s[i][j]='X';
+            }
+        }
+    }
+
+    fo(i,n){
+        cout<<s[i]<<nl;
+    }
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    
+}
+
