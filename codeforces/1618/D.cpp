73a646bdf12d1ef72493ad5239c0ff126fbd184b
Srinjoy Ray <srinjoyray@yahoo.in>
2021-12-14 20:14:00 +0530

    Add solution for problem `D - Array and Operations` Link: https://codeforces.com/contest/1618/problem/D Tags: dp, greedy, math, *1300 Ref: https://codeforces.com/contest/1618/submission/139232178

---
+++ codeforces/1618/D.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+const int N=1e6+10;
+
+void solve(){
+    int i,j;
+    int n,k;
+    cin>>n>>k;
+    int a[n],rem,ans=0;
+    vi v;
+    fo(i,n){
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    // fo(i,n){
+    //     cout<<a[i]<<" ";
+    // }cout<<nl;
+
+    rem=n-2*k;
+    fo(i,n){
+        if(i<rem){
+            ans+=a[i];
+        }
+        else{
+            v.pb(a[i]);
+        }
+    }
+    // deb2(rem,ans);
+    int mx=0,temp;
+    map<int,int> mapp;
+
+    fo(i,v.size()){
+        mapp[v[i]]++;
+        mx=max(mx,mapp[v[i]]);
+    }
+
+    if(mx>v.size()/2){
+        temp=mx-v.size()/2;
+        ans+=temp;
+    }
+
+    cout<<ans<<nl;
+
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    
+}
+
