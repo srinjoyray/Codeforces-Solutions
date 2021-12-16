6267dba4df25a5a5728190fd2b6974602bec5b3f
Srinjoy Ray <srinjoyray@yahoo.in>
2021-12-16 19:09:00 +0530

    Add solution for problem `A - Forbidden Subsequence` Link: https://codeforces.com/contest/1617/problem/A Tags: constructive algorithms, greedy, sortings, strings Ref: https://codeforces.com/contest/1617/submission/139485459

---
+++ codeforces/1617/A.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+const int N=1e6+10;
+
+void solve(){
+    int i,j;
+    string s,t,ans;
+    cin>>s>>t;
+    map<int,int> mapp;
+    fo(i,s.size()){
+        mapp[s[i]-'a']++;
+    }
+    if(t=="abc" && mapp[0]>0){
+        fo(j,mapp[0]){
+            ans.pb('a');
+        }
+        fo(j,mapp[2]){
+            ans.pb('c');
+        }
+        fo(j,mapp[1]){
+            ans.pb('b');
+        }
+    }
+    else{
+        fo(i,3){
+            fo(j,mapp[i]){
+                ans.pb('a'+i);
+            }
+        }
+    }
+    fo2(i,3,25){
+        fo(j,mapp[i]){
+            ans.pb('a'+i);
+        }
+    }
+    cout<<ans<<nl;
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    
+}
+
