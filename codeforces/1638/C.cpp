eb35e65af333182a05df1fcf9d60027ebb552e3a
Srinjoy Ray <srinjoyray@yahoo.in>
2022-02-14 20:28:00 +0530

    Add solution for problem `C - Inversion Graph` Link: https://codeforces.com/contest/1638/problem/C Tags: data structures, dsu, graphs, math, *1300 Ref: https://codeforces.com/contest/1638/submission/146381128

---
+++ codeforces/1638/C.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+const int N=1e6+10;
+
+void solve(){
+    int i,j;
+    int n;
+    cin>>n;
+    int a[n],ans=0,val,mn;
+    fo(i,n){
+        cin>>a[i];
+    }
+    set<int> se;
+    fo2(i,1,n){
+        se.insert(i);
+    }
+    i=n-1;
+    mn=n+1;
+    while(i>=0){
+        val=*se.rbegin();
+        mn=n+1;
+        j=i;
+        // deb2(i,j);
+        while(j>=0){
+            // deb(j);
+            mn=min(mn,a[j]);
+            se.erase(a[j]);
+            // deb2(val,mn);
+            if(se.size()==0 || (val!=*se.rbegin() && val-mn == i-j)){
+                break;
+            }
+            j--;
+        }
+        i=j-1;
+        ans++;
+    }
+
+    cout<<ans<<nl;
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    
+}
+
