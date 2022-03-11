0ebc226c314a6f9929bf4860569bf563b2480211
Srinjoy Ray <srinjoyray@yahoo.in>
2022-03-11 20:58:00 +0530

    Add solution for problem `D - Madoka and the Best School in Russia` Link: https://codeforces.com/contest/1647/problem/D Tags: constructive algorithms, dp, math, number theory, *1900 Ref: https://codeforces.com/contest/1647/submission/149296848

---
+++ codeforces/1647/D.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(i,start,end) for(i=start;i<=end;i++)
+#define rfo(i,n) for(i=n-1;i>=0;i--)
+#define deb(x) cerr<<#x<<"="<<x<<endl
+#define deb2(x,y) cerr<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it,a) for(auto it=a.begin(); it!=a.end();it++)
+#define nl '\n'
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+const int N=1e6+10;
+bool is_prime(ll num){
+    ll i;
+    // if(num==1)return false;
+    for(i=2;i*i<=num;i++){
+        if(num%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+void solve(){
+    int i,j;
+    ll x,d;
+    cin>>x>>d;
+    ll num,count=0;
+    num = x;
+    while(num%d == 0 ){
+        num/=d;
+        count++;
+    }
+    // deb2(num,count);
+    if(count==1){
+        cout<<"NO\n";
+        return;
+    }
+    if(!(is_prime(num))){
+        cout<<"YES\n";
+        return;
+    }
+    
+    if(count>2 && !(is_prime(d)) ){
+        // num is prime
+        if(count==3 && num*num==d){
+            cout<<"NO\n";
+        }
+        else{
+            cout<<"YES\n";
+        }
+        return;
+    }
+    cout<<"NO\n";
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    
+}
+
